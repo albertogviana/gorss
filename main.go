9e133c13426879a7bdf9115247407b07dbea6265
Alberto Viana <albertogviana@gmail.com>
2015-12-12 22:01:54 -0200

    Using goroutines

--- main.go
+++ main.go
@@ -1,44 +1,43 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
-	"encoding/xml"
+	"strings"
 )
 
 const Url = "http://rss.dw.com/xml/DKpodcast_dwn1_pt"
+const Folder = "/Users/alberto/Documents/Projects/docker/gorss/audio/"
 
-type EnclosureTag struct  {
+type EnclosureTag struct {
 	XMLName xml.Name `xml:"enclosure"`
-	Url string `xml:"url,attr"`
+	Url     string   `xml:"url,attr"`
 }
 
 type Item struct {
-	XMLName xml.Name `xml:"item"`
+	XMLName   xml.Name     `xml:"item"`
 	Enclosure EnclosureTag `xml:"enclosure"`
 }
 
 type ChannelTag struct {
 	XMLName xml.Name `xml:"channel"`
-	Item []Item `xml:"item"`
+	Item    []Item   `xml:"item"`
 }
 
 type RssTag struct {
-	XMLName xml.Name `xml:"rss"`
+	XMLName  xml.Name     `xml:"rss"`
 	Channels []ChannelTag `xml:"channel"`
 }
 
-
-func GetSessionList() []byte {
+func GetEpisodeList() []byte {
 	response, err := http.Get(Url)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-
-	fmt.Println(response.StatusCode)
 
 	defer response.Body.Close()
 	content, err := ioutil.ReadAll(response.Body)
@@ -51,16 +50,66 @@ func GetSessionList() []byte {
 	return content
 }
 
-func main() {
-	r := GetSessionList()
+func GetFilename(url string) string {
+	urlSplit := strings.Split(url, "/")
+	return urlSplit[len(urlSplit)-1]
+}
+
+func GetEpisode(url string, downloadChannel chan string) {
+	filename := GetFilename(url)
+
+	fmt.Printf("Getting episode %s \n", filename)
+
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer res.Body.Close()
+	content, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = WriteFile(filename, content)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	downloadChannel <- fmt.Sprintf("Downloaded %s \n", filename)
+}
+
+func WriteFile(filename string, content []byte) error {
+	err := ioutil.WriteFile(Folder + filename, content, 0444)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return err
+}
+
+func HandleDownload() {
+	r := GetEpisodeList()
+
+	downloadChannel := make(chan string)
 
 	var rssTag RssTag
 
 	xml.Unmarshal(r, &rssTag)
 
+	counterEpisodes := 0
 	for _, channel := range rssTag.Channels {
 		for _, item := range channel.Item {
-			fmt.Printf("%s \n", item.Enclosure.Url)
+			go GetEpisode(item.Enclosure.Url, downloadChannel)
+			counterEpisodes++
 		}
 	}
+
+	for i := 0; i < counterEpisodes; i++ {
+		fmt.Printf("%s", <-downloadChannel)
+	}
+}
+
+func main() {
+	HandleDownload()
 }
